1144298624ba7acf6d2ab98da695bf193766163d
Iamm3taphorical <mahirdyan30@gmail.com>
2026-01-11 21:23:59 +0600

    Auto-sync: 2026-01-11 21:23:59

---
+++ leetcode/85. Maximal Rectangle/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximalRectangle(self, matrix: list[list[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == '1':
+                    heights[c] += 1
+                else:
+                    heights[c] = 0
+            
+            # Largest Rectangle in Histogram logic
+            stack = []
+            for i in range(cols + 1):
+                curr_height = heights[i] if i < cols else 0
+                while stack and curr_height < heights[stack[-1]]:
+                    h = heights[stack.pop()]
+                    width = i if not stack else i - stack[-1] - 1
+                    max_area = max(max_area, h * width)
+                stack.append(i)
+                
+        return max_area
